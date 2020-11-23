26173cc61f8cc33dee16eba5af971f825a6f60e2
Takas0522 <t_o_19880522@outlook.com>
2020-11-23 15:49:07 +0900

    add

---
+++ LearnGraphSubscriptionLifeCycleManagement/LearnGraphSubscriptionLifeCycleManagement/LifecycleNotifications.cs
@@ -0,0 +1,36 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace LearnGraphSubscriptionLifeCycleManagement
+{
+    public static class LifecycleNotifications
+    {
+        [FunctionName("LifecycleNotifications")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("LifecycleNotifications Run");
+
+            string token = req.Query["validationToken"];
+            if (token != null)
+            {
+                log.LogInformation($"Return token {token}");
+                return new ContentResult { Content = token, ContentType = "text/plain", StatusCode = 200 };
+            }
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+
+            log.LogInformation(requestBody);
+
+            return new AcceptedResult();
+        }
+    }
+}
---
+++ LearnGraphSubscriptionLifeCycleManagement/README.md
@@ -0,0 +1,33 @@
+Subscription LifeCycle Management
+
+実験コード
+
+異なるEndPoint
+
+```
+POST https://graph.microsoft.com/v1.0/subscriptions
+Content-Type: application/json
+{
+  "changeType": "created,updated",
+  "notificationUrl": "https://webhook.azurewebsites.net/api/resourceNotifications",
+  "lifecycleNotificationUrl": "https://webhook.azurewebsites.net/api/lifecycleNotifications",
+  "resource": "/users/{id}/messages",
+  "expirationDateTime": "2020-03-20T11:00:00.0000000Z",
+  "clientState": "<secretClientState>"
+}
+```
+
+うまくいくパターン？
+
+```
+POST https://graph.microsoft.com/v1.0/subscriptions
+Content-Type: application/json
+{
+  "changeType": "created,updated",
+  "notificationUrl": "https://webhook.azurewebsites.net/api/resourceNotifications",
+  "lifecycleNotificationUrl": "https://webhook.azurewebsites.net/api/lifecycleNotifications",
+  "resource": "/users/{id}/messages",
+  "expirationDateTime": "2020-03-20T11:00:00.0000000Z",
+  "clientState": "<secretClientState>"
+}
+```
